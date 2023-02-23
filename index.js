123b2c1d52f0eaec4334409a5f9b84cf2eda7a68
santoshv <svhanchenje>
2023-02-23 18:34:50 +0530

    finaltest

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-//importing node framework
-var express = require(‘express’);
-var app = express();//Respond with "hello world" for requests that hit our root "/"
-app.get(‘/’, function (req, res) {
- res.send(‘hello world’);
-});//listen to port 80 by default
-app.listen(process.env.PORT || 80);
-module.exports = app;
+// var express = require("express");
+ 
+// var app = express();
+
+//     //Respond with "hello world" for requests that hit our root "/"
+// app.get("/", function (req, res) {​​
+//  res.send("hello world");
+// }​​);
+
+//     //listen to port 3000 by default
+// app.listen(process.env.PORT || 3000);
+ 
+// module.exports = app;
+const http = require('http');
+const hostname = '0.0.0.0';
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World , have u changed?\n');
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
